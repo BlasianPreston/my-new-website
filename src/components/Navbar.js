00cde350479facef0e7b2829d7d0b38c54e6220d
Isiah Williams <84547105+BlasianPreston@users.noreply.github.com>
2025-03-09 00:02:11 -0500

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-import Logo from '../images/big-logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import '../styles/navbar.css'
 
 
@@ -11,6 +10,7 @@ export default function Navbar() {
     return (
         <nav>
             <div className='nav'>
+                <div className='spacer'/>
                 <div className='pages'>
                     <Link className='link' href="/">Home</Link>
                     <Link className='link' href="/about">About Me</Link>
@@ -19,9 +19,9 @@ export default function Navbar() {
                 </div>
                 <div className='icons'>
                     <ul className='socials'>
-                        <li><a href='https://www.instagram.com/isiahw06/' target="_blank"><span><FontAwesomeIcon className='social-media' icon={faInstagram}/></span></a></li>
-                        <li><a href='https://www.linkedin.com/in/preston-williams-72b581261/' target="_blank"><span><FontAwesomeIcon className='social-media' icon={faLinkedin}/></span></a></li>
-                        <li><a href='https://github.com/BlasianPreston' target="_blank"><span><FontAwesomeIcon className='social-media' icon={faGithub}/></span></a></li>
+                        <li><a href='https://www.instagram.com/isiahw06/' target="_blank"><span><FaInstagram/></span></a></li>
+                        <li><a href='https://www.linkedin.com/in/preston-williams-72b581261/' target="_blank"><span><FaLinkedin/></span></a></li>
+                        <li><a href='https://github.com/BlasianPreston' target="_blank"><span><FaGithub/></span></a></li>
                     </ul>
                 </div>
             </div>
