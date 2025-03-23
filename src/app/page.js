af9ac6b69a5cb7b80ea3811a7712bcebac31d506
Isiah Williams <84547105+BlasianPreston@users.noreply.github.com>
2025-03-22 23:29:16 -0400

    added viewport

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import Home from "../components/Home.js";
 import "./style.css";
 
 export default function App() {
   return (
     <div className="main">
+      <Head>
+        <meta name="description" content="This is Preston's personal website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+      </Head>
       <main>
         <Home />
       </main>
